3013c98b212b578f865a86dfef9dcfe8306567ed
Сергей Субботин <zarodiny@yandex.ru>
2023-01-19 22:20:09 +1000

    добавляет форму

--- templates/single-trainings.php
+++ templates/single-trainings.php
@@ -8,70 +8,44 @@ Template Post Type: post
 <main>
     <section class="descriptions">
         <div class="container">
-            <h1>Тренинг «Деньги»</h1>
-            <p>Когда есть деньги — это здорово. Особенно, если их хватает жить безбедно. Можно выражаться в
-                стиле жизни и в своих интересах.
-
-                Когда их много — еще лучше. Можно проявлять их уже в щедрости Души и дальнейших возможностях
-                развития жизни.
-
-                Когда их нет, ситуация двойственная:
-
-                Жизнь к вам небезразлична и мягко пытается направлять вас в те сферы вашего бытия, от которых вы
-                отвернулись в пользу денег.
-                Вы обезумели от нехватки денег и просто уже выпадаете из жизни.
-
-                И где именно ваша мера много-мало, необходимо-достаточно.</p>
-            <p>В сегодняшнем мире сложно получить знания, которые помогли бы решить финансовый вопрос именно
-                Вам. Но это возможно!
-
-                На тренинге вы начнете заново открывать свою жизнь. С нового угла зрения увидите ограничения,
-                препятствия, не позволяющие вам двигаться в выбранном направлении. Откроете ваш природный
-                потенциал, о котором вы или вообще не подозреваете, или оставили его далеко в детстве.
-
-            </p>
+        <h1><?php the_title(); ?></h1>
+            <?php the_post_thumbnail( '' );  ?>
+            <?php the_content(); ?>
         </div>
     </section>
     <div class="when">
         <div class="container">
-            <h2>Ближайший тренинг: 21.11.2020</h2>
+            <h2>Ближайший тренинг: <?= CFS()->get('training_when') ?></h2>
             <p>Количество участников ограничено!</p>
         </div>
     </div>
     <section class="why">
         <div class="container">
-            <h2>Зачем Вам этот тренинг?</h2>
+            <h2><?= CFS()->get('why_title') ?></h2>
             <ul class="why__list">
                 <li class="why__item">
-                    <img src="img/redparus.jpg" alt="">
-                    <p>Вы узнаете истинные причины отсутствия у Вас денег</p>
+                    <img src="<?= CFS()->get('why_card_1_img') ?>" alt="">
+                    <p><?= CFS()->get('why_card_1_text') ?></p>
                 </li>
                 <li class="why__item">
-                    <img src="img/redparus.jpg" alt="">
-                    <p>Вы узнаете, как изменить ситуацию в свою пользу</p>
+                    <img src="<?= CFS()->get('why_card_2_img') ?>" alt="">
+                    <p><?= CFS()->get('why_card_2_text') ?></p>
                 </li>
                 <li class="why__item">
-                    <img src="img/redparus.jpg" alt="">
-                    <p>Вы станете чаще радоваться и перестанете беспокоиться</p>
+                    <img src="<?= CFS()->get('why_card_3_img') ?>" alt="">
+                    <p><?= CFS()->get('why_card_3_text') ?></p>
                 </li>
             </ul>
         </div>
     </section>
     <div class="form">
-        <h2>Записаться на тренинг</h2>
+        <h2><?= CFS()->get('form_title') ?></h2>
         <div class="container">
             <div class="form__wrapper">
-                <form action="#">
-                    <input type="text" class="input" placeholder="Ваше имя">
-                    <input type="tel" class="input" placeholder="Ваш телефон">
-                    <button type="submit" class="submit-btn">
-                        <a class="load-more__btn load-more__btn-one" href="portfolio.html">
-                            <span>оставить заявку</span>
-                        </a>
-                    </button>
-                </form>
+                <?= do_shortcode( CFS()->get('form_shortcode')); ?>
                 <p>* Ваши личные данные будут надежно защищены</p>
             </div>
+
         </div>
 
     </div>
@@ -80,60 +54,22 @@ Template Post Type: post
             <h2>Что говорят о тренинге участники?</h2>
             <div class="training-reviews__slider swiper">
                 <ul class="training-reviews__wrapper swiper-wrapper">
+                <?php $loop = CFS()->get('reviews');
+                foreach ($loop as $row ) {
+                ?>
                     <li class="training-reviews__item swiper-slide">
                         <div class="training-reviews__item-img">
-                            <img src="img/about-300x200.jpg" alt="фото">
+                            <img src="<?= $row['reviews_img'] ?>" alt="<?= $row['	reviews_img_descriptions'] ?>">
                         </div>
                         <div class="training-reviews__item-wrapper">
-                            <p class="training-reviews__item-name">Андрей</p>
-                            <p class="training-reviews__item-text"><q><em> Можно ли за несколько часов круто
-                                        изменить
-                                        свою
-                                        жизнь? Оказывается, можно. Самое удивительное, что ответы находятся в
-                                        нас
-                                        самих.
-                                        Нам
-                                        просто нужен человек, который поможет их увидеть. И Ирина это делает.
-                                        Спасибо за
-                                        тренинг. </em></q></p>
+                            <p class="training-reviews__item-name"><?= $row['reviews_name'] ?></p>
+                            <p class="training-reviews__item-text">
+                                <q><em><?= $row['reviews_text'] ?></em></q></p>
                         </div>
                     </li>
-                    <li class="training-reviews__item swiper-slide">
-                        <div class="training-reviews__item-img">
-                            <img src="img/about-300x200.jpg" alt="фото">
-                        </div>
-                        <div class="training-reviews__item-wrapper">
-                            <p class="training-reviews__item-name">Андрей</p>
-                            <p class="training-reviews__item-text"><q><em> Можно ли за несколько часов круто
-                                        изменить
-                                        свою
-                                        жизнь? Оказывается, можно. Самое удивительное, что ответы находятся в
-                                        нас
-                                        самих.
-                                        Нам
-                                        просто нужен человек, который поможет их увидеть. И Ирина это делает.
-                                        Спасибо за
-                                        тренинг. </em></q></p>
-                        </div>
-                    </li>
-                    <li class="training-reviews__item swiper-slide">
-                        <div class="training-reviews__item-img">
-                            <img src="img/about-300x200.jpg" alt="фото">
-                        </div>
-                        <div class="training-reviews__item-wrapper">
-                            <p class="training-reviews__item-name">Андрей</p>
-                            <p class="training-reviews__item-text"><q><em> Можно ли за несколько часов круто
-                                        изменить
-                                        свою
-                                        жизнь? Оказывается, можно. Самое удивительное, что ответы находятся в
-                                        нас
-                                        самих.
-                                        Нам
-                                        просто нужен человек, который поможет их увидеть. И Ирина это делает.
-                                        Спасибо за
-                                        тренинг. </em></q></p>
-                        </div>
-                    </li>
+                <?php
+                    }
+                ?>
                 </ul>
             </div>
             <div class="swiper-pagination"></div>
