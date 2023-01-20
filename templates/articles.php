c81c98946ab996add2f80d13b449ea4aa783d915
Сергей Субботин <zarodiny@yandex.ru>
2023-01-20 18:02:22 +1000

    добавляет страницу статьи

--- templates/articles.php
+++ templates/articles.php
@@ -10,61 +10,59 @@ Template Name: Статьи
             <h1>Статьи</h1>
             <div class="articles__wrapper">
                 <ul class="articles__list">
-                    <li class="articles__item">
+                <?php
+                        $my_posts = get_posts( array(
+                            'numberposts' => -1,
+                                'category' => 5,
+                                'orderby' => 'title',
+                                'order' => 'ASC',
+                                'post_type'   => 'post',
+                                'suppress_filters' => true,
+                            ) );
+
+                        foreach( $my_posts as $post ){
+                            setup_postdata( $post );
+                        ?>
+
+                        <li class="articles__item">
                         <div class="articles__img">
-                            <img src="img/1.jpg" alt="">
+                            <?php the_post_thumbnail( '' );  ?>
                         </div>
                         <div class="articles__item-text">
-                            <h3>О ДУХЕ…</h3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium facere
-                                inventore
-                                optio
-                                architecto dolor asperiores maiores fugit incidunt autem, quod mollitia corporis
-                                expedita
-                                reprehenderit vel itaque eaque odit tenetur similique numquam illo? Eius
-                                quisquam,
-                                veritatis
-                                odio a
-                                aperiam alias reiciendis.</p>
-                            <a href="#" class="link">читать далее >></a>
+                            <h3><?php the_title(); ?></h3>
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>" class="link">читать далее >></a>
                         </div>
                     </li>
-                    <li class="articles__item">
-                        <div class="articles__img">
-                            <img src="img/1.jpg" alt="">
-                        </div>
-                        <div class="articles__item-text">
-                            <h3>О ДУХЕ…</h3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium facere
-                                inventore
-                                optio
-                                architecto dolor asperiores maiores fugit incidunt autem, quod mollitia corporis
-                                expedita
-                                reprehenderit vel itaque eaque odit tenetur similique numquam illo? Eius
-                                quisquam,
-                                veritatis
-                                odio a
-                                aperiam alias reiciendis.</p>
-                            <a href="#" class="link">читать далее >></a>
-                        </div>
-                    </li>
+                    <?php
+                        }
+                        wp_reset_postdata();
+                    ?>
                 </ul>
                 <div id="articles">
                     <input class="search input input--search" type="text" class="search" placeholder="Поиск">
                     <h4>Статьи на сайте</h4>
                     <ul class="main-articles-list-search list">
-                        <li class="main-articles-item-search">
-                            <a class="link article" href="#">О ДУХЕ…</a>
-                        </li>
-                        <li class="main-articles-item-search">
-                            <a class="link article" href="#">О ДУШЕ…</a>
-                        </li>
-                        <li class="main-articles-item-search">
-                            <a class="link article" href="#">О ДУХОВНОМ РАЗВИТИИ…</a>
-                        </li>
-                        <li class="main-articles-item-search">
-                            <a class="link article" href="#">О СЕКСУАЛЬНОЙ ЭНЕРГИИ…</a>
-                        </li>
+                    <?php
+                                $my_posts = get_posts( array(
+                                    'numberposts' => -1,
+                                        'category' => 5,
+                                        'orderby' => 'title',
+                                        'order' => 'ASC',
+                                        'post_type'   => 'post',
+                                        'suppress_filters' => true,
+                                    ) );
+
+                                foreach( $my_posts as $post ){
+                                    setup_postdata( $post );
+                                ?>
+                                <li class="main-articles-item-search">
+                                    <a class="link article" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </li>
+                            <?php
+                                }
+                                wp_reset_postdata();
+                            ?>
                     </ul>
                 </div>
             </div>
