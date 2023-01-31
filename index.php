784f8b7026e53e0dd6b747970cc8080bed6da7b5
Сергей Субботин <zarodiny@yandex.ru>
2023-01-31 14:04:20 +1000

    добавляет стили

--- index.php
+++ index.php
@@ -46,48 +46,6 @@ Template Name: Главная страница
                 </div>
             </section>
 
-            <section class="training">
-                <div class="container">
-                    <h2>Тренинги</h2>
-                    <p>Тренинги рассчитаны на 2 дня, время проведения с 10.00 до 18.00, количество участников 10-15
-                        человек.</p>
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
-                    <div class="training__wrapper swiper">
-                        <ul class="swiper-wrapper">
-                        <?php
-                            $my_posts = get_posts( array(
-                            'numberposts' => -1,
-                                'category' => 4,
-                                'orderby' => 'title',
-                                'order' => 'ASC',
-                                'post_type'   => 'post',
-                                'suppress_filters' => true,
-                            ) );
-
-                            foreach( $my_posts as $post ){
-                                setup_postdata( $post );
-                            ?>
-
-                            <li class="swiper-slide">
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_post_thumbnail( '' );  ?>
-                                    <h3><?php the_title(); ?></h3>
-                                    <p class="training__when">
-                                        Ближайший тренинг: <time datetime="<?= CFS()->get('training_when_robots') ?>"><?= CFS()->get('training_when') ?></time>
-                                    </p>
-                                    <p class="training__description"><?= CFS()->get('training__descriptions') ?></p>
-                                </a>
-                            </li>
-                            <?php
-                                }
-                            wp_reset_postdata();
-                        ?>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-
             <section class="online">
                 <div class="container">
                     <h2>Онлайн курс</h2>
@@ -133,6 +91,48 @@ Template Name: Главная страница
                         <a class="load-more__btn load-more__btn-one" href="course">
                             <span>подробнее</span>
                         </a>
+                    </div>
+                </div>
+            </section>
+
+            <section class="training">
+                <div class="container">
+                    <h2>Тренинги</h2>
+                    <p>Тренинги рассчитаны на 2 дня, время проведения с 10.00 до 18.00, количество участников 10-15
+                        человек.</p>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+                    <div class="training__wrapper swiper">
+                        <ul class="swiper-wrapper">
+                        <?php
+                            $my_posts = get_posts( array(
+                            'numberposts' => -1,
+                                'category' => 4,
+                                'orderby' => 'title',
+                                'order' => 'ASC',
+                                'post_type'   => 'post',
+                                'suppress_filters' => true,
+                            ) );
+
+                            foreach( $my_posts as $post ){
+                                setup_postdata( $post );
+                            ?>
+
+                            <li class="swiper-slide">
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_post_thumbnail( '' );  ?>
+                                    <h3><?php the_title(); ?></h3>
+                                    <p class="training__when">
+                                        Ближайший тренинг: <time datetime="<?= CFS()->get('training_when_robots') ?>"><?= CFS()->get('training_when') ?></time>
+                                    </p>
+                                    <p class="training__description"><?= CFS()->get('training__descriptions') ?></p>
+                                </a>
+                            </li>
+                            <?php
+                                }
+                            wp_reset_postdata();
+                        ?>
+                        </ul>
                     </div>
                 </div>
             </section>
